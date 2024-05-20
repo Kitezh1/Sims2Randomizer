e84dd209704826daa9792b3253f39bdfdb158163
Patrick Meehan <pjmeehanpj33@gmail.com>
2024-05-20 22:11:55 +0100

    updating for pc use

--- sims.js
+++ sims.js
@@ -1,19 +1,30 @@
-function randNumber(int) {
-    return int;
-}
-
-textContent = aspirations[number];
-
-
 function randAspiration() {
-    const aspirations = ["Pleasure", "Family", "Romance", "Knowledge", "Fortune", "Popularity"];
+    const aspirations = ["pleasure.png", "Family", "Romance", "Knowledge", "Fortune", "Popularity"];
     number = Math.floor(Math.random() * 6);
     return aspirations[number];
 }
 
+function randHobbie() {
+    const hobbies = ["Cuisine", "Film & Literature", "Tinkering", "Sports", "Music & Dance", "Fitness", "Arts & Crafts", "Science", "Games", "Nature"];
+    number = Math.floor(Math.random() * 9);
+    return hobbies[number];
+}
+
+
+// HOBBIES
+const myHobbie = document.getElementById("hobbieRand");
+const hobbieCalc = document.getElementById("hobbieCalc");
+
+hobbieCalc.addEventListener("click", function(e) {
+    myHobbie.innerHTML = randHobbie();
+
+});
+
+
+// ASPIRATIONS
 const myAspiration = document.getElementById("aspirationRand");
+const aspCalc = document.getElementById("aspCalc");
 
-myAspiration.addEventListener("click", function() {
-    randAspiration.innerHTML = randAspiration;
-
+aspCalc.addEventListener("click", function(e) {
+    myAspiration.innerHTML = randAspiration();
 });
